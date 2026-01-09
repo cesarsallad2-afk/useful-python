7c8f1b89c21283765c11e3f327d13047261b240e
cesarsallad2-afk <cesarsallad2@gmail.com>
2026-01-09 15:01:00 +0100

    calc.py
    not done

---
+++ calc.py
@@ -0,0 +1,10 @@
+import math
+
+asterix = "*"
+print("enter: x, +, -, /,") # prints the first custion
+answer = input()
+if answer == ["x" or "X" or asterix]:
+    print("valid")
+else:
+    print("invalid")
+if answer == [""]
